b2793bbc3bb14235e34518e4aaee3854ce461fcd
AliHG <alihg01@gmail.com>
2016-07-21 04:59:18 +0200

    privide an alternative way to display toast messages add xliff tag to strings.xml move onclick attribute from the layout file to styles.xml

--- app/src/main/java/com/example/android/myappportfolio/MainActivity.java
+++ app/src/main/java/com/example/android/myappportfolio/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.myappportfolio;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,51 +14,69 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    /**
-     * Called when the user touches the Popular Movies button
-     */
-    public void openPopularMovies(View view) {
-        // Display a Toast in response to button click
-        Toast.makeText(MainActivity.this, R.string.popular_movies_toast,Toast.LENGTH_SHORT).show();
+//    /**
+//     * Called when the user touches the Popular Movies button
+//     */
+//    public void openPopularMovies(View view) {
+//        // Display a Toast in response to button click
+//        Toast.makeText(MainActivity.this, R.string.popular_movies_toast,Toast.LENGTH_SHORT).show();
+//    }
+//
+//    /**
+//     * Called when the user touches the Stock Hawk button
+//     */
+//    public void openStockHawk(View view) {
+//        // Display a Toast in response to button click
+//        Toast.makeText(MainActivity.this, R.string.stock_hawk_toast,Toast.LENGTH_SHORT).show();
+//    }
+//
+//    /**
+//     * Called when the user touches the BuildItBigger button
+//     */
+//    public void openBuildItBigger(View view) {
+//        // Display a Toast in response to button click
+//        Toast.makeText(MainActivity.this, R.string.build_it_bigger_toast,Toast.LENGTH_SHORT).show();
+//    }
+//
+//    /**
+//     * Called when the user touches the Make Your App Material button
+//     */
+//    public void openMakeYourAppMaterial(View view) {
+//        // Display a Toast in response to button click
+//        Toast.makeText(MainActivity.this, R.string.make_your_app_material_toast,Toast.LENGTH_SHORT).show();
+//    }
+//
+//    /**
+//     * Called when the user touches the Go Ubiquitous button
+//     */
+//    public void openGoUbiquitous(View view) {
+//        // Display a Toast in response to button click
+//        Toast.makeText(MainActivity.this, R.string.go_ubiquitous_toast,Toast.LENGTH_SHORT).show();
+//    }
+//
+//    /**
+//     * Called when the user touches the Capstone button
+//     */
+//    public void openCapstone(View view) {
+//        // Display a Toast in response to button click
+//        Toast.makeText(MainActivity.this, R.string.capstone_toast,Toast.LENGTH_SHORT).show();
+//    }
+
+
+    public void showToast(View view){
+        Button button = (Button) view;
+        String appName = button.getText().toString();
+
+        // show toast message
+        Toast.makeText(MainActivity.this,getString(R.string.toast_message,appName),Toast.LENGTH_SHORT).show();
+
     }
 
     /**
-     * Called when the user touches the Stock Hawk button
+     * another way to display a multiple Toasts
+     * @param resId
      */
-    public void openStockHawk(View view) {
-        // Display a Toast in response to button click
-        Toast.makeText(MainActivity.this, R.string.stock_hawk_toast,Toast.LENGTH_SHORT).show();
-    }
-
-    /**
-     * Called when the user touches the BuildItBigger button
-     */
-    public void openBuildItBigger(View view) {
-        // Display a Toast in response to button click
-        Toast.makeText(MainActivity.this, R.string.build_it_bigger_toast,Toast.LENGTH_SHORT).show();
-    }
-
-    /**
-     * Called when the user touches the Make Your App Material button
-     */
-    public void openMakeYourAppMaterial(View view) {
-        // Display a Toast in response to button click
-        Toast.makeText(MainActivity.this, R.string.make_your_app_material_toast,Toast.LENGTH_SHORT).show();
-    }
-
-    /**
-     * Called when the user touches the Go Ubiquitous button
-     */
-    public void openGoUbiquitous(View view) {
-        // Display a Toast in response to button click
-        Toast.makeText(MainActivity.this, R.string.go_ubiquitous_toast,Toast.LENGTH_SHORT).show();
-    }
-
-    /**
-     * Called when the user touches the Capstone button
-     */
-    public void openCapstone(View view) {
-        // Display a Toast in response to button click
-        Toast.makeText(MainActivity.this, R.string.capstone_toast,Toast.LENGTH_SHORT).show();
+    private void showToastMessage(int resId) {
+        Toast.makeText(MainActivity.this, resId, Toast.LENGTH_SHORT).show();
     }
 }
